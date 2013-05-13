801744631e50d8cd1a14a3684920a6485954a3cd
Digital-Learner <digital.learning.journeysatgmail.com>
2013-05-13 16:47:25 +0100

    Initialize project - setup Cucumber/RSpec + Guard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnowYourCity::Application.config.secret_token = 'be3e2adc4b3e09fdf59ad89e20eacf65951535cf088aafde63146aa3a6982712529d2e9a3d3a46c0b1c6028ba326ede279aec99147161b43aa2dc0076bfdc280'
