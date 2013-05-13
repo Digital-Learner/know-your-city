d9899e777d4157f16eb2d7c09552782e9f1f1adf
Digital-Learner <digital.learning.journeysatgmail.com>
2013-05-13 17:21:37 +0100

    Questions Step 2 - passing

--- features/step_definitions/questions_step_definitions.rb
+++ features/step_definitions/questions_step_definitions.rb
@@ -2,8 +2,8 @@ Given(/^I am on the statements create page$/) do
   visit(new_question_path)
 end
 
-When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |field, value|
+  fill_in field, :with => value
 end
 
 When(/^I choose "(.*?)"$/) do |arg1|
